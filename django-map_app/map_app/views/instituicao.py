b7bdda041b7dd831c91ce3949ad404dc64a35b0e
Matheus Rangel <matheus_rangel@ufrn.edu.br>
2018-11-30 21:57:28 -0300

    Instituicao e orgao list/detail, correção de erros

--- django-map_app/map_app/views/instituicao.py
+++ django-map_app/map_app/views/instituicao.py
@@ -1,18 +1,23 @@
-from map_app.models import Orgao, Instituicao
+from map_app.models import Instituicao, Despesa
 from map_app.forms import InstituicaoForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import DetailView, ListView, CreateView, UpdateView
-
-class InstituicaoListView(ListView):
-    model = Instituicao
-    def get_queryset(self):
-        return Instituicao.objects.all()
+from django.views.generic import DetailView, CreateView, UpdateView
+import datetime
 
 class InstituicaoDetailView(DetailView):
     model = Instituicao
     def get_context_data(self, **kwargs):
+        now = datetime.datetime.now()
         context = super().get_context_data(**kwargs)
-        context['orgao_list'] = Orgao.objects.filter(instituicao = self.kwargs['pk'])
+        context['ano'] = now.year
+        despesas = Despesa.objects.filter(orgao_instituicao__instituicao=self.kwargs['pk'], data_inicio__year=now.year)
+        empenhado = 0
+        gasto = 0
+        for despesa in despesas:
+            empenhado += despesa.empenhado
+            gasto += despesa.pago
+        context['gasto'] = gasto
+        context['empenhado'] = empenhado 
         return context
 
 class InstituicaoCreateView(LoginRequiredMixin, CreateView):
