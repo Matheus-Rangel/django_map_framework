6879de9222511f9eb45f5e504f23d2199e93f63f
joaoei <joaoeizidio@gmail.com>
2018-12-06 09:42:29 -0300

    Info app concluido em todas as instancias

--- vigia_recife/vigia_recife/settings.py
+++ vigia_recife/vigia_recife/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    'atualiza_app'
+    'atualiza_app',
     'map_app',
     'rest_framework',
     'rangefilter',
